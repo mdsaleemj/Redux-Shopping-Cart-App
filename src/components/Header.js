45e5f2e7ba8e88f1c5b52051a68c75e7ae951d45
Saleem <mojowhar@paypal.com>
2022-03-16 11:21:00 +0530

    Adding auth slice and cart slice and cart action redux logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/auth-slice";
 import Cart from "./Cart";
 import "./Header.css";
 const Header = () => {
+  const dispatch = useDispatch();
+  const logOut = () => {
+    dispatch(authActions.logout());
+  };
   return (
     <header>
       <nav className="header-nav">
@@ -16,6 +22,12 @@ const Header = () => {
           </li>
           <li>
             <Cart />
+          </li>
+
+          <li>
+            <button className="logout-btn" onClick={logOut}>
+              Log out
+            </button>
           </li>
         </ul>
       </nav>
